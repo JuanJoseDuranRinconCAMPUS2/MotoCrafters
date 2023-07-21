ba49145a70d22ff0c970ca5af146484d688e552d
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-07-21 07:14:46 -0500

    feat: :sparkles: Creacion del End Point para los cruds de la tabla Marca
    Se crea el end point que contiene los diferentes metodos post, get , update y detele que manipulan y administran la tabla marca de la base de datos. Y ademas se actualiza la version del proyecto en package.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 import dotnev from 'dotenv';
 import express from 'express';
 import CColores from './routes/CrudColores.js';
+import CMarca from './routes/CrudMarca.js';
 console.clear();
 dotnev.config();
 
 const MotoApi = express();
 MotoApi.use(express.json());
 MotoApi.use("/Colores", CColores);
+MotoApi.use("/Marcas", CMarca)
 
 const config = JSON.parse(process.env.MY_CONFIG);
 MotoApi.listen(config, ()=>{console.log(`http://${config.hostname}:${config.port}`);})
---
+++ backend/routes/CrudMarca.js
@@ -0,0 +1,81 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2';
+
+import {Router} from 'express';
+const CMarca= Router();
+dotenv.config();
+let con = undefined;
+CMarca.use((req,res,next)=>{
+    let myconfig = JSON.parse(process.env.MY_CONNECT);
+    con = mysql.createPool(myconfig);
+    next();
+})
+CMarca.get('/GetMarcas', (req,res)=>{
+    con.query(
+        /*SQL*/`SELECT * FROM Marca`,
+        (err,data,fil)=>{
+            if (data.length == 0) {
+                const errorMessage = `No hay data disponible en esta BD`;
+                res.status(500).send(errorMessage);
+            } else {
+                data = JSON.stringify(data);
+                res.send(JSON.parse(data));
+            }
+        }
+    );
+})
+CMarca.post('/PostMarcas', (req,res)=>{
+    const { nombreMarca } = req.body;
+    con.query(
+        /*SQL*/`INSERT INTO Marca (Mrc_Nombre) VALUES(?);`,
+        [nombreMarca],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al enviar la data`;
+                res.status(500).send(errorMessage, err.message);
+            } else {
+                res.send("La data a sido enviada correctamente");
+            }
+        }
+    );
+})
+CMarca.put('/PutMarcas', (req,res)=>{
+    const idMarca = req.query.idMarca;
+    const {nombreMarca} = req.body;
+    con.query(
+        /*SQL*/`UPDATE Marca SET Mrc_Nombre = ? WHERE Mrc_Id = ?;`,
+        [nombreMarca, idMarca],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al actualizar la data`;
+                res.status(500).send(errorMessage);
+            } else {
+                if (data.affectedRows === 0) {
+                    res.status(404).send("La marca con el ID especificado no existe.");
+                } else {
+                    res.send("Los datos han sido actualizados correctamente");
+                }
+            }
+        }
+    );
+})
+CMarca.delete('/DeleteMarcas', (req,res)=>{
+    const idMarca = req.body.IdDelete;
+    con.query(
+        /*SQL*/`DELETE FROM Marca WHERE Mrc_Id = ?;`,
+        [idMarca],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al borrar la data`;
+                res.status(500).send(errorMessage);
+            } else {
+                if (data.affectedRows === 0) {
+                    res.status(404).send("La marca con el ID especificado no existe.");
+                } else {
+                    res.send("Los datos han sido borrados correctamente");
+                }
+            }
+        }
+    );
+})
+export default CMarca;
