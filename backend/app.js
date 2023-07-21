4358e8a31a5d25233534ebf7d3efb9a2c9831ee0
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-07-20 22:54:17 -0500

    feat: :sparkles: Creacion del End Point para los cruds de la tabla Colores
    Se crea el end point que contiene los diferentes metodos post, get , update y detele que manipulan y administran la tabla colores de la base de datos. Y ademas se crea la configuacion de Typescript para validar en siguientes commits

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 import dotnev from 'dotenv';
 import express from 'express';
-
+import CColores from './routes/CrudColores.js';
 console.clear();
 dotnev.config();
 
 const MotoApi = express();
 MotoApi.use(express.json());
-// MotoApi.use("/prueba");
+MotoApi.use("/Colores", CColores);
 
 const config = JSON.parse(process.env.MY_CONFIG);
 MotoApi.listen(config, ()=>{console.log(`http://${config.hostname}:${config.port}`);})
---
+++ backend/routes/CrudColores.js
@@ -0,0 +1,93 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2';
+
+import {Router} from 'express';
+const CColores= Router();
+dotenv.config();
+let con = undefined;
+CColores.use((req,res,next)=>{
+    let myconfig = JSON.parse(process.env.MY_CONNECT);
+    con = mysql.createPool(myconfig);
+    next();
+})
+CColores.get('/GetColores', (req,res)=>{
+    con.query(
+        /*SQL*/`SELECT * FROM Colores`,
+        (err,data,fil)=>{
+            if (data.length == 0) {
+                const errorMessage = `Ninguno`;
+                res.status(500).send(errorMessage);
+            } else {
+                data = JSON.stringify(data);
+                res.send(JSON.parse(data));
+            }
+        }
+    );
+})
+CColores.post('/PostColores', (req,res)=>{
+    const valoresColores = Object.values(req.body);
+    const coloresJSON = {};    
+    valoresColores.forEach((color, index) => {
+        const colorKey = `color${index + 1}`;
+        coloresJSON[colorKey] = color;
+      });
+    const JsonData = JSON.stringify(coloresJSON);
+    con.query(
+        /*SQL*/`INSERT INTO Colores (Cl_DisColores) VALUES(?);`,
+        [JsonData],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al enviar la data`;
+                res.status(500).send(errorMessage, err.message);
+            } else {
+                res.send("La data a sido enviada correctamente");
+            }
+        }
+    );
+})
+CColores.put('/PutColores', (req,res)=>{
+    const valoresColores = Object.values(req.body);
+    const idColor = req.query.idColor;
+    const coloresJSON = {};    
+    valoresColores.forEach((color, index) => {
+        const colorKey = `color${index + 1}`;
+        coloresJSON[colorKey] = color;
+      });
+    const JsonData = JSON.stringify(coloresJSON);
+    con.query(
+        /*SQL*/`UPDATE Colores SET Cl_DisColores = ? WHERE Cl_Id = ?;`,
+        [JsonData, idColor],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al actualizar la data`;
+                res.status(500).send(errorMessage);
+            } else {
+                if (data.affectedRows === 0) {
+                    res.status(404).send("El color con el ID especificado no existe.");
+                } else {
+                    res.send("Los datos han sido actualizados correctamente");
+                }
+            }
+        }
+    );
+})
+CColores.delete('/DeleteColores', (req,res)=>{
+    const idColor = req.body.IdDelete;
+    con.query(
+        /*SQL*/`DELETE FROM Colores WHERE Cl_Id = ?;`,
+        [idColor],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al borrar la data`;
+                res.status(500).send(errorMessage);
+            } else {
+                if (data.affectedRows === 0) {
+                    res.status(404).send("El color con el ID especificado no existe.");
+                } else {
+                    res.send("Los datos han sido borrados correctamente");
+                }
+            }
+        }
+    );
+})
+export default CColores;
