3c457beddc72f9cb0bc2d4a5de65554400092009
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-07-21 07:52:30 -0500

    feat: :sparkles: Creacion del End Point para los cruds de la tabla Tipo_Motocicleta
    Se crea el end point que contiene los diferentes metodos post, get , update y detele que manipulan y administran la tabla de tipos de motocicletas de la base de datos. Y ademas se actualiza la version del proyecto en package.js

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,19 @@ import dotnev from 'dotenv';
 import express from 'express';
 import CColores from './routes/CrudColores.js';
 import CMarca from './routes/CrudMarca.js';
+import CTiposMotocicletas from './routes/CrudTiposMotocicletas.js';
 console.clear();
 dotnev.config();
 
 const MotoApi = express();
 MotoApi.use(express.json());
+
+// Endpoint con los cruds de las tablas de la base de datos
+// ════════ ⋆★⋆ ════════
 MotoApi.use("/Colores", CColores);
-MotoApi.use("/Marcas", CMarca)
+MotoApi.use("/Marcas", CMarca);
+MotoApi.use("/TiposMotocicletas", CTiposMotocicletas);
+// ════════ ⋆★⋆ ════════
 
 const config = JSON.parse(process.env.MY_CONFIG);
 MotoApi.listen(config, ()=>{console.log(`http://${config.hostname}:${config.port}`);})
---
+++ backend/routes/CrudTiposMotocicletas.js
@@ -0,0 +1,97 @@
+import dotenv from 'dotenv';
+import mysql from 'mysql2';
+
+import {Router} from 'express';
+const CTiposMotocicletas= Router();
+dotenv.config();
+let con = undefined;
+CTiposMotocicletas.use((req,res,next)=>{
+    let myconfig = JSON.parse(process.env.MY_CONNECT);
+    con = mysql.createPool(myconfig);
+    next();
+})
+// metodo get
+// ━━━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━━━
+CTiposMotocicletas.get('/GetTiposMotocicletas', (req,res)=>{
+    con.query(
+        /*SQL*/`SELECT * FROM Tipo_Motocicleta`,
+        (err,data,fil)=>{
+            if (data.length == 0) {
+                const errorMessage = `No hay data disponible en esta BD`;
+                res.status(500).send(errorMessage);
+            } else {
+                data = JSON.stringify(data);
+                res.send(JSON.parse(data));
+            }
+        }
+    );
+})
+// ━━━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━━━
+
+// metodo post
+// ━━━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━━━
+CTiposMotocicletas.post('/PostTiposMotocicletas', (req,res)=>{
+    const { nombreTipo } = req.body;
+    con.query(
+        /*SQL*/`INSERT INTO Tipo_Motocicleta (Tpo_Nombre) VALUES(?);`,
+        [nombreTipo],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al enviar la data`;
+                res.status(500).send(errorMessage, err.message);
+            } else {
+                res.send("La data a sido enviada correctamente");
+            }
+        }
+    );
+})
+// ━━━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━━━
+
+// metodo put
+// ━━━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━━━
+CTiposMotocicletas.put('/PutTiposMotocicletas', (req,res)=>{
+    const idTipos = req.query.idTipos;
+    const {nombreTipo} = req.body;
+    con.query(
+        /*SQL*/`UPDATE Tipo_Motocicleta SET Tpo_Nombre = ? WHERE Tpo_Id = ?;`,
+        [nombreTipo, idTipos],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al actualizar la data`;
+                res.status(500).send(errorMessage);
+            } else {
+                if (data.affectedRows === 0) {
+                    res.status(404).send(`el tipo de motocicleta con el ID ${idTipos} no existe.`);
+                } else {
+                    res.send("Los datos han sido actualizados correctamente");
+                }
+            }
+        }
+    );
+})
+// ━━━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━━━
+
+// metodo delete
+// ━━━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━━━
+CTiposMotocicletas.delete('/DeleteTiposMotocicletas', (req,res)=>{
+    const idTipos = req.body.IdDelete;
+    con.query(
+        /*SQL*/`DELETE FROM Tipo_Motocicleta WHERE Tpo_Id = ?;`,
+        [idTipos],
+        (err,data,fil)=>{
+            if (err) {
+                const errorMessage = `Error al borrar la data`;
+                res.status(500).send(errorMessage);
+            } else {
+                if (data.affectedRows === 0) {
+                    res.status(404).send(`el tipo de motocicleta con el ID ${idTipos} no existe.`);
+                } else {
+                    res.send("Los datos han sido borrados correctamente");
+                }
+            }
+        }
+    );
+})
+// ━━━━━━━━━━━━ ◦ ❖ ◦ ━━━━━━━━━━━━
+
+export default CTiposMotocicletas;
