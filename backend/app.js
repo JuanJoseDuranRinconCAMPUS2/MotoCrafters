0e9951ffb51813ed7c0269fafed99de5498f06dd
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-07-22 15:00:59 -0500

    feat: :sparkles: Creacion del End Point para los cruds de la tabla Grupo_Engine
    Se crea el end point que contiene los diferentes metodos post, get , update y delete que manipulan y administran la tabla de los grupos Engine las motocicletas de la base de datos. Y ademas se actualiza la version del proyecto en package.json

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ import CTiposMotocicletas from './routes/CrudTiposMotocicletas.js';
 import CCategoriasMotocicletas from './routes/CrudCategoriasMotocicletas.js';
 import CMotocicletas from './routes/CrudMotocicletas.js';
 import CGrupoFrame from './routes/CrudGrupoFrame.js';
+import CGrupoEngine from './routes/CrudGrupoEngine.js';
+CGrupoEngine
 console.clear();
 dotnev.config();
 
@@ -20,6 +22,7 @@ MotoApi.use("/TiposMotocicletas", CTiposMotocicletas);
 MotoApi.use("/CategoriasMotocicletas", CCategoriasMotocicletas);
 MotoApi.use("/Motocicletas", CMotocicletas);
 MotoApi.use("/GrupoFrame", CGrupoFrame);
+MotoApi.use("/GrupoEngine", CGrupoEngine);
 // ════════ ⋆★⋆ ════════
 
 const config = JSON.parse(process.env.MY_CONFIG);
